e86277ccbacf0e24c454467b03ff9ca98e04069f
Dinesh Singh Dhami <dineshdhamidn@gmail.com>
2025-12-29 16:17:05 +0545

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Image(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to="myimage")
-    
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Image {self.id} - {self.likes} likes"
