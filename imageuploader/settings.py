f4d1be03962954b79266d36cb82c277a3a90f905
Dinesh Singh Dhami <dineshdhamidn@gmail.com>
2025-12-29 20:09:39 +0545

    Update settings.py

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,13 @@ SECRET_KEY = 'django-insecure-%t@$psw4#bc2p=cxymy4^1(+n_vag29j%d0u*7he=+s#pqbq)a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'yourphotoyourfame.pythonanywhere.com',  # PythonAnywhere
+    '127.0.0.1:8000',
+     '127.0.0.1',                                               # Localhost
+    'localhost',                         # Localhost
+]
+
 
 
 # Application definition
@@ -119,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
